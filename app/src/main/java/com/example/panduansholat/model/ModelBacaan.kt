292f02a72b9457efc2722f929ac6e81718a789f5
ArdyPramudyatama <bellinakenisha@gmail.com>
2022-10-28 01:41:46 +0700

    aplikasi panduan sholat

---
+++ app/src/main/java/com/example/panduansholat/model/ModelBacaan.kt
@@ -0,0 +1,9 @@
+package com.example.panduansholat.model
+
+class ModelBacaan {
+    var id: String? = null
+    var name: String? = null
+    var arabic: String? = null
+    var latin: String? = null
+    var terjemahan: String? = null
+}
